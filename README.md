0820693a477f12b1d89a44257eda51eab5d71d7c
phrichards <phrichards@gmail.com>
2014-10-24 16:00:46 -0400

    updated readme

--- README.md
+++ README.md
@@ -6,6 +6,24 @@ The animations are those found in [SpinKit](https://github.com/tobiasahlin/SpinK
 
 ## Options
 
-Spinner Number: Choose one of eight available animations.
+spinnerNumber: Choose one of eight available animations.
 
-Background Color: Choose the color of the animation.
+backgroundColor: Choose the color of the animation.
+
+top & left: CSS top and left values can be passed in to change the positioning of the animations on the page.
+
+## Usage
+
+* Add the class of "loading" to your body tag.
+* Add the following tag above your closing body tag: 
+	
+		<div class="overlay"></div>
+
+* To call the plugin, attach it to the element that triggers your Ajax request. To use it with a button, for example, you'd use code like the following:
+
+		$('myButton').on('click', function(){
+			$(this).loaders({spinnerNumber: 6, backgroundColor: 'purple', top: '10%', left: '90%'});
+			myAjaxCall();
+		});	
+
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,53 +1,44 @@
 $(function() {
 
-	var getTracks = function(){
+	var mockAjax = function(){
 		$.ajax({
-			url: 'http://ws.audioscrobbler.com/2.0/',
-			type: 'GET',
-			dataType: 'jsonp',
-			data: {
-				method: 'user.getartisttracks',
-				user: 'elevature',
-				artist: 'the national',
-				api_key: '51657a939714920b12e608782d9961d0',
-				format: 'json'
-			},
-			success: function(result) {
-				console.log(result);
+			url: '/test/inline',
+			dataType: 'json',
+			success: function() {
 			}
-		}); // end of ajax
-	} // end of getTracks
+		});
+	}
 
 	$('.button1').on('click', function(){
 		$(this).loaders({spinnerNumber: 1, backgroundColor: 'green'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button2').on('click', function(){
 		$(this).loaders({spinnerNumber: 2, backgroundColor: 'blue'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button3').on('click', function(){
 		$(this).loaders({spinnerNumber: 3, backgroundColor: 'red'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button4').on('click', function(){
 		$(this).loaders({spinnerNumber: 4, backgroundColor: 'yellow'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button5').on('click', function(){
 		$(this).loaders({spinnerNumber: 5, backgroundColor: 'orange', top: '10%', left: '10%'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button6').on('click', function(){
 		$(this).loaders({spinnerNumber: 6, backgroundColor: 'purple', top: '10%', left: '90%'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button7').on('click', function(){
 		$(this).loaders({spinnerNumber: 7, backgroundColor: 'brown', top: '60%', left: '10%'});
-		getTracks();
+		mockAjax();
 	});
 	$('.button8').on('click', function(){
 		$(this).loaders({spinnerNumber: 8, backgroundColor: 'black', top: '60%', left: '90%'});
-		getTracks();
+		mockAjax();
 	});
 });
